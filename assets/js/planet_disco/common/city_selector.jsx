95e24a8767243fe7fb1ddc15c50e513678b41aa6
Aleksei <alexey.kashuba97@gmail.com>
2020-06-22 01:46:10 +0200

    fix city selector in safari

--- assets/js/planet_disco/common/city_selector.jsx
+++ assets/js/planet_disco/common/city_selector.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { TextField, CircularProgress } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import { Autocomplete } from '@material-ui/lab'
-import { useQuery } from '@apollo/react-hooks'
+import React, { useMemo, useState, useEffect, useContext } from 'react'
 import { gql } from 'apollo-boost'
 import { StoreContext } from './store'
+// import throttle from 'lodash/throttle';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { useApolloClient } from '@apollo/react-hooks'
+
 
 const CITIES = gql`query CitiesAutocomplete($term: String) {
   cities(q: $term, limit: 10) {
@@ -17,64 +22,107 @@ const CITIES = gql`query CitiesAutocomplete($term: String) {
   }
 }`
 
-const useStyles = makeStyles((theme) => ({
-  header: {
-    marginBottom: theme.spacing(2)
-  },
-}))
 
-export default () => {
-  const classes = useStyles()
-  const [options, setOptions] = useState([])
-  const { dispatch } = useContext(StoreContext)
-  const { loading, data, refetch } = useQuery(CITIES)
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function GoogleMaps() {
+  const classes = useStyles();
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState([]);
+  const graphql = useApolloClient()
+  const { state: { city }, dispatch } = useContext(StoreContext)
+
+  const fetch = useMemo(
+    (() =>
+      debounce(((request, callback) => {
+        graphql.query({
+          query: CITIES,
+          variables: { term: request }
+        }).then((res) => callback(res))
+      }), 200, false)),
+    [],
+  );
 
   useEffect(() => {
-    data && setOptions(data.cities.entries)
-  }, [data])
+    let active = true;
 
-  function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+    fetch(inputValue, (results) => {
+      if (active) {
+        let newOptions = [];
+
+        if (city) {
+          newOptions = [city];
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results.data.cities.entries];
+        }
+
+        setOptions(newOptions);
+      }
+    });
+
+    return () => {
+      active = false;
     };
-  };
+  }, [inputValue, fetch]);
 
-  return <Autocomplete
-    filterOptions={(_, input) => {
-      refetch({term: input.inputValue})
-      return options
-    }}
-    autoComplete={false}
-    getOptionSelected={(option, value) => option.id === value.id}
-    getOptionLabel={(option) => `${option.city}, ${option.humanCountry}`}
-    onChange={(_, city) => dispatch({type: 'SET_CITY', city})}
-    options={options}
-    loading={loading}
-    className={classes.header}
-    ListboxProps={{className: classes.search}}
-    renderInput={(params) => (
-      <TextField
-        {...params}
-        label="Pick a city to learn more..."
-        InputProps={{
-          ...params.InputProps,
-          endAdornment: (
-            <>
-              {loading && <CircularProgress color="inherit" size={20} />}
-              {params.InputProps.endAdornment}
-            </>
-          ),
-        }}
-      />
-    )}
-  />
+  return (
+    <Autocomplete
+      id="city-selector"
+      getOptionLabel={(option) => `${option.city}, ${option.humanCountry}`} 
+      getOptionSelected={(option, value) => option.id === value.id}
+      filterOptions={(x) => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={city}
+      onChange={(_, newValue) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        dispatch({ type: 'SET_CITY', city: newValue })
+      }}
+      onInputChange={(_, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Pick a city to learn more..." variant="outlined" fullWidth />
+      )}
+      renderOption={(option) => {
+        return (
+          <Grid container alignItems="center">
+            <Grid item>
+              <LocationOnIcon className={classes.icon} />
+            </Grid>
+            <Grid item xs>
+              {option.city}
+
+              <Typography variant="body2" color="textSecondary">
+                {option.humanCountry}
+              </Typography>
+            </Grid>
+          </Grid>
+        );
+      }}
+    />
+  );
 }
