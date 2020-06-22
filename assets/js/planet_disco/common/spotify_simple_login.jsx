669667161d682bdbb61e34878a99a61e5ef2a90d
Aleksei <alexey.kashuba97@gmail.com>
2020-06-22 03:57:51 +0200

    redirect to the previous page after login

--- assets/js/planet_disco/common/spotify_simple_login.jsx
+++ assets/js/planet_disco/common/spotify_simple_login.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
-import React from 'react'
+import React, { useRef, useState, useContext, useEffect } from 'react'
 import { Paper, Box } from '@material-ui/core'
-import {Redirect} from 'react-router'
+import { Redirect } from 'react-router'
+import { makeStyles } from '@material-ui/core/styles'
+import { StoreContext } from '../common/store'
 
-const styles = theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: "100%",
     width: "100%",
@@ -22,7 +23,7 @@ const styles = theme => ({
     align: "center",
     backgroundColor: 'rgba(71, 80, 98, 0.8)',
   },
-});
+}));
 
 const clientId = "6371ef37c4fc48a18b52a3837f1b51a9"
 
@@ -36,47 +37,59 @@ function getAccessToken() {
   return token;
 }
 
-class SpotifySimpleLogin extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { classes } = this.props;
-    let accessToken = getAccessToken()
-
-    if (accessToken){
-      window.localStorage.setItem('access_token', accessToken)
-    }
-
-    return (
-      <React.Fragment>
-        {accessToken ?
-          <Redirect to='/' />
-          :
-          <Paper className={classes.root}>
-            <div style={{ width: "auto", margin: "0 auto" }}>
-              <a href={`https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=http:%2F%2Flocalhost:4000/login&scope=&response_type=token&state=`}>
-                <Button
-                  type="button"
-                  variant="contained"
-                  color="primary"
-                >
-                  Log in with Spotify
-              </Button>
-              </a>
-            </div>
-          </Paper>
-        }
-      </React.Fragment>
-    );
-  }
+function encodeState(location, city) {
+  let state = { location: location, city: city }
+  return encodeURIComponent(JSON.stringify(state))
 }
 
+function getCurrentLocation() {
+  var parser = document.createElement('a');
+  parser.href = window.location;
+  return parser.pathname;
+}
 
-SpotifySimpleLogin.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+export default function SpotifySimpleLogin() {
+  const { state: { city }, dispatch } = useContext(StoreContext)
+  const [redirect, setRedirect] = useState(null)
+  const newState = useRef(null)
 
-export default withStyles(styles)(SpotifySimpleLogin);
+  const classes = useStyles()
+  let accessToken = getAccessToken()
+  let location = getCurrentLocation()
+
+  useEffect(() => {
+    if (accessToken) {
+      window.localStorage.setItem('access_token', accessToken)
+      let state = JSON.parse(decodeURIComponent(getParameterByName("state")))
+      if (state && state.city) {
+        newState.current = state;
+        dispatch({ type: "SET_CITY", city: state.city })
+        setRedirect(newState.current.location)
+      } else {
+        setRedirect("/")
+      }
+    }
+  })
+
+  return (
+    <React.Fragment>
+      {accessToken ?
+        redirect && < Redirect to={redirect} />
+        :
+        <Paper className={classes.root}>
+          <div style={{ width: "auto", margin: "0 auto" }}>
+            <a href={`https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=http:%2F%2Flocalhost:4000/login&scope=&response_type=token&state=${encodeState(location, city)}`}>
+              <Button
+                type="button"
+                variant="contained"
+                color="primary"
+              >
+                Log in with Spotify
+              </Button>
+            </a>
+          </div>
+        </Paper>
+      }
+    </React.Fragment>
+  );
+}
